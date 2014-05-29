a478bbdfb443b25abfdd5cab8d08fc67578497d1
Kyle Oliveira <kyle.oliveira@cornell.edu>
2014-05-29 16:14:50 -0400

    [KFSQA-732] Did some preliminary work, but we are going to majorly revise this test.

--- lib/kuality-kfs/page_objects/purap/shop_catalog_page.rb
+++ lib/kuality-kfs/page_objects/purap/shop_catalog_page.rb
@@ -1,6 +1,6 @@
 class ShopCatalogPage < KFSBasePage
 
-#REQUISITION DETAIL
+  #REQUISITION DETAIL
   action(:go_button) { |b| b.frm.button(value: 'Go' ).click } #there is space after search
   element(:key_words) { |b| b.frm.text_field(id: 'Keywords', title: 'Search terms') }
   element(:search_results) { |b| b.frm.table(class: 'SearchResults') }
@@ -14,4 +14,13 @@ class ShopCatalogPage < KFSBasePage
   element(:doc_summary) { |b| b.frm.tds(class: 'DocSummaryBox') }
   action(:doc_po_link) { |b| b.frm.a(text: 'Purchase Order').click }
   action(:attachments_link) { |b| b.frm.a(text: 'Attachments').click }
+
+  element(:panel_right) { |b| b.frm.divs(class: 'HomeShopPanelRight') }
+  element(:showcased_services_section) { |b| b.panel_right.td(text: 'Showcased Services').parent.parent } # Should be the selector div
+  action(:showcased_services) { |i=0, b| b.showcased_services_section.buttons[i] }
+  element(:hosted_suppliers_section) { |b| b.panel_right[b.panel_right.to_a.index { |d| d.text.match(/Hosted Suppliers/m) }] } # Should be the selector div
+  action(:hosted_suppliers_buttons) { |b| b.hosted_suppliers_section.div(id: "container-#{b.hosted_suppliers_section.id}").table.tds(class: 'sticker') }
+  action(:hosted_suppliers) { |i=0, b| b.hosted_suppliers_buttons[i].images.first }
+  element(:punch_out_section) { |b| b.panel_right.td(text: 'Punch-out').parent.parent } # Should be the selector div
+  action(:punch_out) { |i=0, b| b.punch_out_section.buttons[i] }
 end
