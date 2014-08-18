2df704ca610c9ec377f4b31ba368aa3681768448
Nancy Kimble <nkk4@cornell.edu>
2014-08-18 12:38:57 -0400

    [KFSQA-1012] Changes required for code review and KFS code changes.

--- lib/kuality-kfs/page_objects/labor_distribution/salary_expense_transfer_page.rb
+++ lib/kuality-kfs/page_objects/labor_distribution/salary_expense_transfer_page.rb
@@ -12,28 +12,28 @@ class SalaryExpenseTransferPage < FinancialProcessingPage
   action(:return_selected) { |b| b.frm.button(id: /methodToCall.prepareToReturnSelectedResults/m).click }
 
   #Accounting Lines (READ-ONLY data elements)
-  value(:chart_code_value) { |t='source', i=0, b| b.frm.span(id: 'document.#{t}AccountingLine[#{i}].chartOfAccountsCode.div').text.strip }
-  value(:account_number_value) { |t='source', i=0, b| b.frm.span(id: 'document.#{t}AccountingLine[#{i}].accountNumber.div').text.strip }
-  value(:sub_account_number_value) { |t='source', i=0, b| b.frm.span(id: 'document.#{t}AccountingLine[#{i}].subAccountNumber.div').text.strip }
-  value(:object_code_value) { |t='source', i=0, b| b.frm.span(id: 'document.#{t}AccountingLine[#{i}].financialObjectCode.div').text.strip }
-  value(:sub_object_code_value) { |t='source', i=0, b| b.frm.span(id: 'document.#{t}AccountingLine[#{i}].financialSubObjectCode.div').text.strip }
-  value(:project_code_value) { |t='source', i=0, b| b.frm.span(id: 'document.#{t}AccountingLine[#{i}].projectCode.div').text.strip }
-  value(:position_number_value) { |t='source', i=0, b| b.frm.span(id: 'document.#{t}AccountingLine[#{i}].positionNumber.div').text.strip }
-  value(:organization_reference_id_value) { |t='source', i=0, b| b.frm.span(id:  'document.#{t}AccountingLine[#{i}].organizationReferenceId.div').text.strip }
-  value(:payroll_end_date_fiscal_year_value) { |t='source', i=0, b| b.frm.span(id: 'document.#{t}AccountingLine[#{i}].payrollEndDateFiscalYear.div').text.strip }
-  value(:payroll_end_date_fiscal_period_code_value) { |t='source', i=0, b| b.frm.span(id: 'document.#{t}AccountingLine[#{i}].payrollEndDateFiscalPeriodCode.div').text.strip }
+  value(:chart_code_value) { |t='source', i=0, b| b.frm.span(id: "document.#{t}AccountingLine[#{i}].chartOfAccountsCode.div").text.strip }
+  value(:account_number_value) { |t='source', i=0, b| b.frm.span(id: "document.#{t}AccountingLine[#{i}].accountNumber.div").text.strip }
+  value(:sub_account_number_value) { |t='source', i=0, b| b.frm.span(id: "document.#{t}AccountingLine[#{i}].subAccountNumber.div").text.strip }
+  value(:object_code_value) { |t='source', i=0, b| b.frm.span(id: "document.#{t}AccountingLine[#{i}].financialObjectCode.div").text.strip }
+  value(:sub_object_code_value) { |t='source', i=0, b| b.frm.span(id: "document.#{t}AccountingLine[#{i}].financialSubObjectCode.div").text.strip }
+  value(:project_code_value) { |t='source', i=0, b| b.frm.span(id: "document.#{t}AccountingLine[#{i}].projectCode.div").text.strip }
+  value(:position_number_value) { |t='source', i=0, b| b.frm.span(id: "document.#{t}AccountingLine[#{i}].positionNumber.div").text.strip }
+  value(:organization_reference_id_value) { |t='source', i=0, b| b.frm.span(id:  "document.#{t}AccountingLine[#{i}].organizationReferenceId.div").text.strip }
+  value(:payroll_end_date_fiscal_year_value) { |t='source', i=0, b| b.frm.span(id: "document.#{t}AccountingLine[#{i}].payrollEndDateFiscalYear.div").text.strip }
+  value(:payroll_end_date_fiscal_period_code_value) { |t='source', i=0, b| b.frm.span(id: "document.#{t}AccountingLine[#{i}].payrollEndDateFiscalPeriodCode.div").text.strip }
   #Accounting Lines (Editable data elements only on this page)
-  value(:st_chart_code) { |t='target', i=0, b| b.frm.select(name: 'document.#{t}AccountingLine[#{i}].chartOfAccountsCode').value}
-  value(:st_account_number) { |t='target', i=0, b| b.frm.text_field(name: 'document.#{t}AccountingLine[#{i}].accountNumber').value.strip }
-  value(:st_sub_account_code) { |t='target', i=0, b| b.frm.text_field(name: 'document.#{t}AccountingLine[#{i}].subAccountNumber').value.strip }
-  value(:st_object_code) { |t='target', i=0, b| b.frm.text_field(name: 'document.#{t}AccountingLine[#{i}].financialObjectCode').value.strip }
-  value(:st_sub_object_code) { |t='target', i=0, b| b.frm.text_field(name: 'document.#{t}AccountingLine[#{i}].financialSubObjectCode').value.strip }
-  value(:st_project_code) { |t='target', i=0, b| b.frm.text_field(name: 'document.#{t}AccountingLine[#{i}].projectCode').value.strip }
-  value(:st_organization_reference_id) { |t='target', i=0, b| b.frm.text_field(name: 'document.#{t}AccountingLine[#{i}].organizationReferenceId').value.strip }
-  value(:st_payroll_total_hours) { |t='source', i=0, b| b.frm.text_field(name: 'document.#{t}AccountingLine[#{i}].payrollTotalHours').value.strip }
-  value(:st_amount) { |t='source', i=0, b| b.frm.text_field(name: 'document.#{t}AccountingLine[#{i}].amount').value.strip }
+  value(:st_chart_code) { |t='target', i=0, b| b.frm.select(name: "document.#{t}AccountingLine[#{i}].chartOfAccountsCode").value}
+  value(:st_account_number) { |t='target', i=0, b| b.frm.text_field(name: "document.#{t}AccountingLine[#{i}].accountNumber").value.strip }
+  value(:st_sub_account_code) { |t='target', i=0, b| b.frm.text_field(name: "document.#{t}AccountingLine[#{i}].subAccountNumber").value.strip }
+  value(:st_object_code) { |t='target', i=0, b| b.frm.text_field(name: "document.#{t}AccountingLine[#{i}].financialObjectCode").value.strip }
+  value(:st_sub_object_code) { |t='target', i=0, b| b.frm.text_field(name: "document.#{t}AccountingLine[#{i}].financialSubObjectCode").value.strip }
+  value(:st_project_code) { |t='target', i=0, b| b.frm.text_field(name: "document.#{t}AccountingLine[#{i}].projectCode").value.strip }
+  value(:st_organization_reference_id) { |t='target', i=0, b| b.frm.text_field(name: "document.#{t}AccountingLine[#{i}].organizationReferenceId").value.strip }
+  value(:st_payroll_total_hours) { |t='source', i=0, b| b.frm.text_field(name: "document.#{t}AccountingLine[#{i}].payrollTotalHours").value.strip }
+  value(:st_amount) { |t='source', i=0, b| b.frm.text_field(name: "document.#{t}AccountingLine[#{i}].amount").value.strip }
   #Accounting Lines (Clickable link)
-  action(:view_fringe_benefit_link) { |t='source', i=0, b| b.frm.span(id: 'document.#{t}AccountingLine[#{i}].fringeBenefitView.div').link(href: /calculateFringeBenefit/) }
+  action(:view_fringe_benefit_link) { |t='source', i=0, b| b.frm.span(id: "document.#{t}AccountingLine[#{i}].fringeBenefitView.div").link(href: /calculateFringeBenefit/) }
 
   #specify :source or :target when requesting count
   action(:accounting_lines_row_count) do |t, b|
