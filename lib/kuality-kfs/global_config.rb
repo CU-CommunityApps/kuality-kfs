2160528c27992ac0904474aec63924e9c8186686
Nancy Kimble <nkk4@cornell.edu>
2014-12-19 11:34:29 -0500

    [KFSQA-1103] Added code to lowercase the user id (netid) so that the validation would always succeed when an uppercase value is passed in.

--- lib/kuality-kfs/global_config.rb
+++ lib/kuality-kfs/global_config.rb
@@ -308,16 +308,18 @@ module GlobalConfig
   def perform_backdoor_login(user)
     # do not continue, required parameter not sent
     fail ArgumentError, 'Required parameter "user" was not specified for perform_backdoor_login.' if user.nil?
+    #ensure lowercase for user id as we will be attempting to find it as the Impersonating User
+    user_lowercase = user.downcase
 
     visit (MainPage)
     on BackdoorLoginPage do |backdoorPage|
-      backdoorPage.username.fit user
+      backdoorPage.username.fit user_lowercase
       backdoorPage.login
 
       #Verify that the requested login actually happened and fail when it doesn't
-      (backdoorPage.login_info_readonly.include? user).should be true
+      (backdoorPage.login_info_readonly.include? user_lowercase).should be true
 
-      set_current_user(user)
+      set_current_user(user_lowercase)
     end
   end
 
