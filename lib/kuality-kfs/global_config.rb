67918046dd98a055fadecb492fac359c95fc4530
Nancy Kimble <nkk4@cornell.edu>
2015-01-06 13:54:03 -0500

    [KFSQA-1103] Code change to address intermittent Selenium WebDriver failure when attempting to backdoor in as specific users for the tests.

--- lib/kuality-kfs/global_config.rb
+++ lib/kuality-kfs/global_config.rb
@@ -306,7 +306,21 @@ module GlobalConfig
   end
 
   def perform_backdoor_login(user)
-    visit(BackdoorLoginPage).login_as(user)
-    set_current_user(user)
+    # do not continue, required parameter not sent
+    fail ArgumentError, 'Required parameter "user" was not specified for perform_backdoor_login.' if user.nil?
+    #ensure lowercase for user id as we will be attempting to find it as the Impersonating User
+    user_lowercase = user.downcase
+
+    visit (MainPage)
+    on BackdoorLoginPage do |backdoorPage|
+      backdoorPage.username.fit user_lowercase
+      backdoorPage.login
+
+      #Verify that the requested login actually happened and fail when it doesn't
+      (backdoorPage.impersonating_user.include? user_lowercase).should be true
+
+      set_current_user(user_lowercase)
+    end
   end
+
 end
--- lib/kuality-kfs/page_objects/backdoor_login_page.rb
+++ lib/kuality-kfs/page_objects/backdoor_login_page.rb
@@ -1,7 +1,13 @@
 class BackdoorLoginPage < BasePage
+
   page_url "#{$base_url}backdoorlogin.do"
 
-  element(:username) { |b| b.text_field(name: 'backdoorId')}
-  action(:login_as) { |user_id, b| b.username.set user_id; b.login}
+  element(:username) { |b| b.frm.text_field(name: 'backdoorId')}
+
+  element(:login_info) { |b| b.div(id: 'login-info') }
+  value(:logged_in_user) { |b| ((b.login_info.strongs)[0]).text.strip }
+  value(:impersonating_user) { |b| (b.login_info.strongs)[1].text.strip }
+
   button 'Login'
+
 end
