e9a3c1696985971efc62416047a1844439ba287c
Nancy Kimble <nkk4@cornell.edu>
2015-02-24 12:53:27 -0500

    [KFSQA-1112] Changes to fix intermittent failures in Sub-Account Tests. Adjusted feature file step wording to better conform to existing step wording. Moved CU specific steps from the foundation project to the CU project. Alphabetized contents  of file [parameter_constants.rb] to make it easier to find entries. Added new parameters for hard coded data values. Adjusted step coding to conform to the standard way of setting up the option hashes passed into the data object create and edit methods. Removed steps that were not being called at all.

--- features/step_definitions/chart_of_accounts/sub_account.rb
+++ features/step_definitions/chart_of_accounts/sub_account.rb
@@ -1,8 +1,3 @@
-And /^I Create a Sub-Account with the default Sub-Account Type Code$/ do
-  type_code = get_aft_parameter_value(ParameterConstants::DFAULT_SUB_ACCOUNT_TYPE_CODE)
-  @sub_account = create SubAccountObject, type_code: type_code, press: :save
-end
-
 When /^I tab away from the Account Number field$/ do
   on SubAccountPage do |page|
     page.account_number.select
@@ -36,54 +31,6 @@ And /^The Sub-Account document should be in my action list$/ do
     page.result_item(@sub_account.document_id).should exist
   end
 end
-
-And /^I (#{SubAccountPage::available_buttons}) a Sub-Account with an adhoc approver$/ do |button|
-  #The requirement for @KFSQA-589 is C&G processor as adhoc user
-  @adhoc_user = get_random_principal_name_for_role('KFS-SYS', 'Contracts & Grants Processor')
-  @user_id = @adhoc_user # for actionlist check
-  account_number = ""
-  i = 0
-  while account_number.empty? && i < 10
-    # must be an account that can have subaccounttype of 'CS'
-    sub_account_info = get_kuali_business_object('KFS-COA','SubAccount','active=true&a21SubAccount.subAccountTypeCode=CS&chartOfAccountsCode=' + get_aft_parameter_value(ParameterConstants::DEFAULT_CHART_CODE))
-    account_number = sub_account_info['accountNumber'][0]
-    account_info = get_kuali_business_object('KFS-COA','Account',"accountNumber=#{account_number}")
-    # check if account is closed or expired
-    if account_info['closed'][0] == 'true'
-      account_number = ""
-    else
-      if (account_info['accountExpirationDate'][0]) != 'null' && DateTime.strptime(account_info['accountExpirationDate'][0], '%Y-%m-%d') < DateTime.now
-        account_number = ""
-      end
-    end
-    i += 1
-  end
-
-  options = {
-      account_number:                      account_number,
-      cost_sharing_chart_of_accounts_code: get_aft_parameter_value(ParameterConstants::DEFAULT_CHART_CODE_WITH_NAME),
-      sub_account_type_code:               'CS',
-      cost_sharing_account_number:         account_number,
-      adhoc_approver_userid:               @adhoc_user,
-      press:                               button.gsub(' ', '_')
-  }
-
-  @sub_account = create SubAccountObject, options
-end
-
-And /^I create a Sub-Account with Sub-Account Type Code (\w+)$/ do |sub_account_type|
-  # only certain subfundgroupcode are acceptable for CS subaccount 'APFEDL' is one of them
-  account_number =   get_kuali_business_object('KFS-COA','Account',"subFundGroupCode=APFEDL&active=Y&accountExpirationDate=NULL&chartOfAccountsCode=#{get_aft_parameter_value(ParameterConstants::DEFAULT_CHART_CODE)}")['accountNumber'].sample
-  options = {
-      account_number:                      account_number,
-      cost_sharing_chart_of_accounts_code: get_aft_parameter_value(ParameterConstants::DEFAULT_CHART_CODE),
-      sub_account_type_code:               sub_account_type,
-      cost_sharing_account_number:         account_number,
-      press:                               'save'
-  }
-  @sub_account = create SubAccountObject, options
-end
-
 
 And /^I lookup the Sub-Account I want to edit$/ do
   #get the sub-account we want to edit
--- lib/kuality-kfs/data_objects/chart_of_accounts/sub_account_object.rb
+++ lib/kuality-kfs/data_objects/chart_of_accounts/sub_account_object.rb
@@ -10,24 +10,22 @@ class SubAccountObject < KFSDataObject
                 :cost_sharing_account_number, :cost_sharing_chart_of_accounts_code, :cost_sharing_sub_account_number,
                 #== Edit CG ICR tab ==#
                 :icr_identifier, :icr_type_code, :icr_off_campus_indicator,
-                #== ==#
+                #== ==#\
                 :adhoc_approver_userid   #TODO This singleton data element is a collection on the page and needs to be coded as such.
 
 #add if needed                :fin_reporting_chart_code, :fin_reporting_org_code, :fin_reporting_code,
 
-  def initialize(browser, opts={})
-    @browser = browser
-
-    defaults = {
-        description:          random_alphanums(40, 'AFT'),
-        chart_code:           get_aft_parameter_value(ParameterConstants::DEFAULT_CHART_CODE),
-        account_number:       get_aft_parameter_value(ParameterConstants::DEFAULT_ACCOUNT_NUMBER),
-        sub_account_number:   random_alphanums(5),
-        name:                 random_alphanums(10),
-        press:                :save
-    }
-    set_options(defaults.merge(default_icr_accounts).merge(get_aft_parameter_values_as_hash(ParameterConstants::DEFAULTS_FOR_SUB_ACCOUNT)).merge(opts))
+  def defaults
+    super.merge({
+        chart_code:             get_aft_parameter_value(ParameterConstants::DEFAULT_CHART_CODE),
+        account_number:         get_aft_parameter_value(ParameterConstants::DEFAULT_ACCOUNT_NUMBER),
+        sub_account_number:     random_alphanums(5),
+        name:                   random_alphanums(10),
+        sub_account_type_code:  get_aft_parameter_value(ParameterConstants::DEFAULT_EXPENSE_SUB_ACCOUNT_TYPE_CODE)
+    }).merge(default_icr_accounts)
+      .merge(get_aft_parameter_values_as_hash(ParameterConstants::DEFAULTS_FOR_SUB_ACCOUNT))
   end
+
 
   def build
     visit(MainPage).sub_account
@@ -44,6 +42,7 @@ class SubAccountObject < KFSDataObject
       add_adhoc_approver(page) unless @adhoc_approver_userid.nil?
     end
   end
+
 
   # Note: You'll need to update the subcollections separately.
   def edit(opts={})
@@ -67,6 +66,18 @@ class SubAccountObject < KFSDataObject
     update_options(on(SubAccountPage).send("sub_account_data_#{target.to_s}"))
     update_line_objects_from_page!(target)
   end
+
+
+  # Class Methods:
+  class << self
+
+    # Attributes that are required for a successful save/submit.
+    # @return [Array] List of Symbols for attributes that are required
+    def required_attributes
+      superclass.required_attributes | [ :chart_code, :account_number, :name, :sub_account_number, :sub_account_type_code]
+    end
+
+  end #class<<self
 
   include IndirectCostRecoveryLinesMixin
 
