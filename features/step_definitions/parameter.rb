a7f5fa5b9044e5ca44b55b7a981588650335b176
Nancy Kimble <nkk4@cornell.edu>
2015-03-02 12:27:42 -0500

    [KFSQA-1113] Committing set of incomplete changes.

--- features/step_definitions/parameter.rb
+++ features/step_definitions/parameter.rb
@@ -3,26 +3,25 @@ And /^I find a value for a parameter named (.*) for the (.*) document$/ do |para
   @parameter_values = on(ParameterPage).parameter_value.value.split(";")
 end
 
-And /^I update the (.*) Parameter for the (.*) component in the (.*) namespace with the following values:$/ do |parameter_name, component, namespace_code, table|
-  update_values = table.rows_hash
 
+And /^I (update|restore) the (.*) Parameter for the (.*) component in the (.*) namespace with the values (.*)$/ do |action, parameter_name, component, namespace_code, parameter_values|
   step "I lookup the #{parameter_name} Parameter for the #{component} component in the #{namespace_code} namespace"
-  on ParameterPage do |page|
-    @parameter = make ParameterObject, namespace_code:                    page.old_namespace_code,
-                                       component:                         page.old_component,
-                                       application_id:                    page.old_application_id,
-                                       parameter_name:                    page.old_parameter_name,
-                                       parameter_value:                   page.old_parameter_value,
-                                       parameter_description:             page.old_parameter_description,
-                                       parameter_type_code:               page.old_parameter_type_code,
-                                       parameter_constraint_code_allowed: page.old_parameter_constraint_code == 'Allowed' ? :set : :clear,
-                                       parameter_constraint_code_denied:  page.old_parameter_constraint_code == 'Denied' ? :set : :clear
 
-    @parameter.edit description: 'Temporary change to add in IC value.',
-                    parameter_value: update_values['Parameter Value'],
-                    press: :submit
+  @parameter = make ParameterObject
+  @parameter.absorb! :old
+
+  options = {
+        description:            'Temporary change for AFT value.',
+        parameter_description:  'Temporary change for automated functional tests.',
+        parameter_value:        @parameter.update_values_remembering_original(parameter_values)
+  }
+
+  @parameter.edit options
+  on ParameterPage do |page|
+    @parameter.document_id = page.document_id
   end
 end
+
 
 And /^I lookup the (.*) Parameter for the (.*) component in the (.*) namespace$/ do |parameter_name, component, namespace_code|
   visit(AdministrationPage).parameter
@@ -35,6 +34,7 @@ And /^I lookup the (.*) Parameter for the (.*) component in the (.*) namespace$/
   end
 end
 
+
 And /^I finalize the Parameter document$/ do
   # Note: this means you'll change users before the end of this step.
   step 'the Parameter document goes to ENROUTE'
@@ -44,8 +44,20 @@ And /^I finalize the Parameter document$/ do
   step 'the Parameter document goes to FINAL'
 end
 
+
 # This step causes the global hash @test_input_data to come into existence.
 # This is a Hash of the Parameter Values in the Parameter table for the specified Parameter Name.
 Given /^I obtain (.*) data values required for the test from the Parameter table$/ do |parameter_name|
   @test_input_data = get_aft_parameter_values_as_hash(parameter_name)
+end
+
+
+When /^I update an application Parameter to allow revenue object codes on a Pre-Encumbrance document$/ do
+  system_parameter = get_aft_parameter_values_as_hash(ParameterConstants::DEFAULT_PRE_ENCUMBRANCE_REVENUE_OBJECT_CODE_PARAMETER_AND_VALUE)
+  step "I update the #{system_parameter[:parameter_name]} Parameter for the #{system_parameter[:component]} component in the #{system_parameter[:namespace_code]} namespace with the values #{system_parameter[:parameter_value]}"
+end
+
+
+And /^I restore the application parameter to its original value$/ do
+  step "I update the #{@parameter.parameter_name} Parameter for the #{@parameter.component} component in the #{@parameter.namespace_code} namespace with the values #{@parameter.original_parameter_value}"
 end
--- lib/kuality-kfs/data_objects/administration/configuration/parameters/ParameterObject.rb
+++ lib/kuality-kfs/data_objects/administration/configuration/parameters/ParameterObject.rb
@@ -1,18 +1,41 @@
-class ParameterObject < MaintenanceObject
+class ParameterObject < KFSDataObject
 
   attr_accessor :namespace_code, :component, :application_id,
                 :parameter_name, :parameter_value, :parameter_description,
-                :parameter_type_code, :parameter_constraint_code_allowed, :parameter_constraint_code_denied
+                :parameter_type_code, :parameter_constraint_code_allowed, :parameter_constraint_code_denied,
 
-  def initialize(browser, opts={})
-    @browser = browser
-    defaults = { description: random_alphanums(40, 'AFT') }
-    set_options(defaults.merge(opts))
+                #during a test a parameter value may need to to be changed, this attribute is used to hold the
+                #original value for restoration at the end of the test
+                :original_parameter_value
+
+
+  def defaults
+    super.merge({
+                  namespace_code:                     '',
+                  component:                          '',
+                  application_id:                     '',
+                  parameter_name:                     '',
+                  parameter_description:              '',
+                  parameter_type_code:                '',
+                  parameter_constraint_code_allowed:  :clear,
+                  parameter_constraint_code_denied:   :clear
+    })
   end
+
+
+  def edit(opts={})
+    on ParameterPage do |page|
+      edit_fields opts, page, :description, :parameter_value, :parameter_description, :parameter_type_code,
+                              :parameter_constraint_code_allowed, :parameter_constraint_code_denied
+    end
+    sync_parameter_constraint_code
+  end
+  alias_method :update, :edit
+
 
   def build
     visit(AdministrationPage).parameter
-    prep_page
+    sync_parameter_constraint_code
     on ParameterPage do |page|
       fill_out page, :description, :namespace_code, :component, :application_id,
                      :parameter_name, :parameter_value, :parameter_description,
@@ -20,13 +43,66 @@ class ParameterObject < MaintenanceObject
     end
   end
 
-  # Edit the Parameter. Note: You'll need to already be on the Parameter's edit page.
-  def update(opts={})
-    prep_page
-    on ParameterPage do |page|
-      edit_fields opts, page, :description, :parameter_value, :parameter_description,:parameter_type_code,
-                              :parameter_constraint_code_allowed, :parameter_constraint_code_denied
+
+  def absorb!(target=:new)
+    super
+    on(ParameterPage) do |page|
+      page.expand_all
+      case target
+        when :new
+          @namespace_code                    = page.new_namespace_code
+          @component                         = page.new_component
+          @application_id                    = page.new_application_id
+          @parameter_name                    = page.new_parameter_name
+          @parameter_value                   = page.new_parameter_value
+          @parameter_description             = page.new_parameter_description
+          @parameter_type_code               = page.new_parameter_type_code
+          @parameter_constraint_code_allowed = page.new_parameter_constraint_code == 'Allowed' ? :set : :clear
+          @parameter_constraint_code_denied  = page.new_parameter_constraint_code == 'Denied' ? :set : :clear
+
+        when :old
+          @namespace_code                    = page.old_namespace_code
+          @component                         = page.old_component
+          @application_id                    = page.old_application_id
+          @parameter_name                    = page.old_parameter_name
+          @parameter_value                   = page.old_parameter_value
+          @parameter_description             = page.old_parameter_description
+          @parameter_type_code               = page.old_parameter_type_code
+          @parameter_constraint_code_allowed = page.old_parameter_constraint_code == 'Allowed' ? :set : :clear
+          @parameter_constraint_code_denied  = page.old_parameter_constraint_code == 'Denied' ? :set : :clear
+      end
     end
   end
+
+
+  # We want the parameter constraint codes to either be :set or nil but only one can
+  # be :set at a time since this is a radio. This should update the other when one is set.
+  def sync_parameter_constraint_code
+    if @parameter_constraint_code_allowed == :set
+      @parameter_constraint_code_denied = :clear
+    elsif @parameter_constraint_code_denied == :set
+      @parameter_constraint_code_allowed = :clear
+    end
+  end
+
+
+  def update_values_remembering_original(value)
+    @original_parameter_value = @parameter_value
+    @parameter_value = value.to_s.gsub(',', ';')
+  end
+
+
+  # Class Methods:
+  class << self
+
+    # Attributes that are required for a successful save/submit.
+    # @return [Array] List of Symbols for attributes that are required
+    def required_attributes
+      superclass.required_attributes | [ :namespace_code, :component, :application_id, :parameter_name,
+                                         :parameter_description, :parameter_type_code,
+                                         :parameter_constraint_code_allowed, :parameter_constraint_code_denied]
+    end
+
+  end #class<<self
 
 end
