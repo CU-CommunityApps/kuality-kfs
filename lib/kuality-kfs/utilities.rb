185d232396708f56d497c5e922a53a914b1f3952
Nancy Kimble <nkk4@cornell.edu>
2015-08-10 14:37:16 -0400

    [KFSQA-1154] Fix failing AFT KFSQA-905. Webservice returns data in two different formats depending on whether one or multiple accounts are returned. Need to deal with both conditions as well as not data being returned. Changed criteria used to look-up an account used for edit action.

--- lib/kuality-kfs/utilities.rb
+++ lib/kuality-kfs/utilities.rb
@@ -145,6 +145,8 @@ module Utilities
         ((get_kuali_business_objects('KFS-COA','Account',"chartOfAccountsCode=#{get_aft_parameter_value(ParameterConstants::DEFAULT_CHART_CODE)}&accountTypeCode=CC&subFundGroup.fundGroupCode=CG&accountName=*cost share*&}")['org.kuali.kfs.coa.businessobject.Account']).sample)['accountNumber'][0]
       when 'General Appropriated Account'
         ((get_kuali_business_objects('KFS-COA','Account',"chartOfAccountsCode=#{get_aft_parameter_value(ParameterConstants::DEFAULT_CHART_CODE)}&accountTypeCode=CC&subFundGroup.fundGroupCode=GN&subFundGroupCode=GNAPPR&}")['org.kuali.kfs.coa.businessobject.Account']).sample)['accountNumber'][0]
+      when 'Contracts & Grants Account'
+        ((get_kuali_business_objects('KFS-COA','Account',"chartOfAccountsCode=#{get_aft_parameter_value(ParameterConstants::DEFAULT_CHART_CODE)}&accountTypeCode=CC&subFundGroup.fundGroupCode=CG&subFundGroupCode=CGFEDL&}")['org.kuali.kfs.coa.businessobject.Account']).sample)['accountNumber'][0]
       when 'Random Sub-Fund Group Code'
         get_kuali_business_object('KFS-COA','Account',"chartCode=#{get_aft_parameter_value(ParameterConstants::DEFAULT_CHART_CODE)}&subFundGroupCode=*&extension.programCode=*&closed=N&extension.appropriationAccountNumber=*&active=Y&accountExpirationDate=NULL")['accountNumber'].sample
       else
